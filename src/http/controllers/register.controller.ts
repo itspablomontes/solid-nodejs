6e6bbf93b7e7c2d98d68e7bb6937845712639cdb
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-23 01:28:51 -0300

    feat: add dependency inversion in controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,7 @@
-import { prisma } from "@/config/database";
 import { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
+import { RegisterService } from "../services/register.service";
+import { PrismaUsersRepository } from "../repositories/prisma/prisma.users.repository";
 
 export const register = async (
   request: FastifyRequest,
@@ -14,13 +15,17 @@ export const register = async (
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const registerService = new RegisterService(usersRepository);
+
+    await registerService.execute({ name, email, password });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send();
+    }
+    return reply.status(500).send();
+  }
 
   return reply.status(201).send();
 };
