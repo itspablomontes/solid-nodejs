2dd3c6f4d63284bdac7dbb5e6eb9787789bc1790
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-23 01:29:02 -0300

    feat: add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
-import z from "zod";
-import { prisma } from "./config/database";
-import { register } from "@/http/controllers/register.controller";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation Error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: "Internal Server Error" });
+});
---
+++ src/http/errors/user.already.exists.ts
@@ -0,0 +1,5 @@
+class UserAlreadyExistsError extends Error {
+  constructor() {
+    super("E-mail Already Exists");
+  }
+}
