1cf124432d8e4adb1383ba16c13d96232fff879e
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-22 23:49:19 -0300

    feat: add query exhibit on db action on dev env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,15 @@
+import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: env.NODE_ENV === "dev" ? ["query"] : [],
+  });
 
 if (process.env.NODE_ENV === "production") {
   globalForPrisma.prisma === prisma;
