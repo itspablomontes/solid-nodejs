4531998beedf2e744fb9e65c5930494b92aecf27
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-23 01:28:23 -0300

    feat: add user repository prisma implementation

---
+++ src/http/repositories/prisma/prisma.users.repository.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/config/database";
+import { Prisma, User } from "@prisma/client";
+import { IUsersRepository } from "../../interfaces/user.repository.interface";
+
+export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+    return user;
+  }
+  async getByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+}
+
+export const prismaUsersRepository = new PrismaUsersRepository();
