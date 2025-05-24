322ee30ace9338707af05223d51c28b5a308edbc
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-24 09:32:50 -0300

    feat: add factory pattern to authenticate modules

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -1,8 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { AuthenticateService } from "../services/authenticate.service";
 import { z } from "zod";
-import { PrismaUsersRepository } from "../repositories/prisma/prisma.users.repository";
 import { InvalidCredentialsError } from "../errors/invalid.credentials";
+import { makeAuthenticateService } from "../services/factories/make.authenticate.service";
 
 export const authenticate = async (
   request: FastifyRequest,
@@ -16,8 +15,7 @@ export const authenticate = async (
   const { email, password } = authenticateRequestSchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const authenticateService = new AuthenticateService(usersRepository);
+    const authenticateService = makeAuthenticateService();
 
     await authenticateService.execute({ email, password });
   } catch (error) {
