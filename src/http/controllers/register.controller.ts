7fa0e6cbfb6893ce64dbc894324884a3be5b4137
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-24 09:33:33 -0300

    feat: add factory pattern to register  modules

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,8 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
-import { RegisterService } from "../services/register.service";
-import { PrismaUsersRepository } from "../repositories/prisma/prisma.users.repository";
 import { UserAlreadyExistsError } from "../errors/user.already.exists";
+import { makeRegisterService } from "../services/factories/make.register.service";
 
 export const register = async (
   request: FastifyRequest,
@@ -17,8 +16,7 @@ export const register = async (
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerService = new RegisterService(usersRepository);
+    const registerService = makeRegisterService();
 
     await registerService.execute({ name, email, password });
   } catch (error) {
