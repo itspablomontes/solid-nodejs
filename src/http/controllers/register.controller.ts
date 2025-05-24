7a8929dc058e40f0762f75aed45b06158a10d0ed
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-24 01:21:20 -0300

    fix: throw error on register controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 import { RegisterService } from "../services/register.service";
 import { PrismaUsersRepository } from "../repositories/prisma/prisma.users.repository";
+import { UserAlreadyExistsError } from "../errors/user.already.exists";
 
 export const register = async (
   request: FastifyRequest,
@@ -24,7 +25,7 @@ export const register = async (
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send();
     }
-    return reply.status(500).send();
+    throw Error;
   }
 
   return reply.status(201).send();
