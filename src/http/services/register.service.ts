86f19853f5e2e96c44611e62b5eae38f123e14c9
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-23 23:43:57 -0300

    chore: add response interface to register service

--- src/http/services/register.service.ts
+++ src/http/services/register.service.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/config/database";
 import { hash } from "bcryptjs";
-import { prismaUsersRepository } from "../repositories/prisma/prisma.users.repository";
+import { UserAlreadyExistsError } from "../errors/user.already.exists";
 import { IUsersRepository } from "../interfaces/user.repository.interface";
+import { User } from "@prisma/client";
 
 interface RegisterServiceRequest {
   name: string;
@@ -9,9 +9,17 @@ interface RegisterServiceRequest {
   password: string;
 }
 
+interface RegisterServiceResponse {
+  user: User;
+}
+
 export class RegisterService {
   constructor(private usersRepository: IUsersRepository) {}
-  async execute({ email, name, password }: RegisterServiceRequest) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.getByEmail(email);
@@ -20,10 +28,14 @@ export class RegisterService {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
